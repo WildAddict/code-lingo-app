47b61cad166c2d2172785cb296b18c66243dee1e
WildAddict <your_email@example.com>
2026-07-01 16:18:03 +0300

    Первая версия моего Mini App

---
+++ bot.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
+import asyncio
+
+TOKEN = "ТВОЙ_ТОКЕН_БОТА"
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+
+    web_app_info = WebAppInfo(url="https://твой-сайт.рф/webapp/index.html")
+    button = InlineKeyboardButton(text="Открыть CodeLingo", web_app=web_app_info)
+    markup = InlineKeyboardMarkup(inline_keyboard=[[button]])
+    
+    await message.answer("Привет! Готов учить немецкие термины?", reply_markup=markup)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
